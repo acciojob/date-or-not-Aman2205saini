876696a2f7a2fe1b6e5dd8d354b89bea9757acfa
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-19 20:44:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 var isDate = function (input) {
   //   write your code here
+	 // If the input is a Date object, check if it is valid
+    if (input instanceof Date) {
+        return !isNaN(input.getTime()); // A valid date will have a valid timestamp
+    }
+
+    // If the input is a string, try to parse it into a Date
+    if (typeof input === 'string') {
+        const parsedDate = new Date(input);
+        return !isNaN(parsedDate.getTime()); // Check if the parsed date is valid
+    }
+
+    // If the input is neither a Date object nor a valid string, return false
+    return false;
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+// const input = prompt("Enter Date.");
+// alert(isDate(input));
